3bb20fd9cb201e2c01b681c12a339787fd06fc06
Slaven B <slaven.bunijevac@gmail.com>
2022-03-10 22:44:03 +0100

    rename variable to english versions

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-class Automobili {
+class Cars {
   constructor(manufacturer, engineType, consumption) {
     this.manufacturer = manufacturer;
     this.engineType = engineType;
     this.consumption = consumption;
   }
 
-  drive(vozioKilometara) {
-    console.log(`${this.manufacturer} je prešao ${vozioKilometara} kilometara.`);
+  drive(kilometersDriven) {
+    console.log(`${this.manufacturer} je prešao ${kilometersDriven} kilometara.`);
   }
 
-  fuelConsumption(kilometri) {
-    this.drive(kilometri);
-    const potrošenoGoriva = this.consumption * (kilometri / 100);
-    console.log(`Potrošeno ${this.engineType}a: ${potrošenoGoriva}.`);
+  fuelConsumption(kilemeters) {
+    this.drive(kilemeters);
+    const fuelSpent = this.consumption * (kilemeters / 100);
+    console.log(`Potrošeno ${this.engineType}a: ${fuelSpent}.`);
   }
 }
 
-const audi = new Automobili('Audi', 'dizel', 6);
-const renoMegan = new Automobili('Reno', 'dizel', 4.3);
-const golf7 = new Automobili('Golf 7', 'benzin', 9);
+const audi = new Cars('Audi', 'dizel', 6);
+const renoMegan = new Cars('Reno', 'dizel', 4.3);
+const golf7 = new Cars('Golf 7', 'benzin', 9);
 
 audi.fuelConsumption(40);
 renoMegan.fuelConsumption(40);
